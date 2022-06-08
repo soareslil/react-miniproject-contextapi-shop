5973c2c78cfb09948d245248bf67bcb0af936e6b
Liliane Soares <78764415+soareslil@users.noreply.github.com>
2022-06-08 16:00:38 -0300

    project done

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,10 +1,41 @@
-import React, { useState } from "react";
+import React from "react";
+import CartContext from "../CartContext";
+import { useContext } from "react";
+import { IoShirtOutline } from "react-icons/io5";
+import { AiFillShopping } from "react-icons/ai";
+import "./styles/card.css";
 
-function Checkout(){
-    return(
-        <div className="checkout">
-            <h3>checkout</h3>
-        </div>
-    )
+function Checkout() {
+  const { items } = useContext(CartContext);
+
+  return (
+    <div>
+      <h3>Checkout</h3>
+      <div className="checkout">
+        {items.map((item) => (
+          <div>
+            <div className="product-list">
+              <div className="formato">
+                <div className="card">
+                  <IoShirtOutline className="icon-shirt" />
+                  <div className="name-and-icon">
+                    <div className="product-name">
+                      <h3>{item.name}</h3>
+                    </div>
+                    <div className="icon-purchase">
+                      <AiFillShopping />
+                    </div>
+                  </div>
+                  <div className="product-price">
+                    <h4>${item.price}</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 export default Checkout;
